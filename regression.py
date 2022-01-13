691f48aa6efa9efe363237285ef05962419ef3ff
NGÔ QUỐC BẢO <67365937+ngoquocbao17@users.noreply.github.com>
2022-01-13 11:25:37 +0700

    Add files via upload

--- regression.py
+++ regression.py
@@ -55,83 +55,91 @@ mae = mean_absolute_error(predict_df['y_test'], predict_df['y_predict'])
 # Part 2: Show project's result with Streamlit
 
 # Hiển thị
-st.title("Data Science")
-st.write('## Price Regression Project')
+# CURRENT_THEME = "green"
+st.image("y4a.png",width=250)
+st.write('# Supply Chain Planning Team')
+st.write("### Sell-in Price Prediction Project")
 
-menu = ['Overview', 'Build Project', 'New Prediction']
-choice = st.sidebar.selectbox('Menu', menu)
-if choice == 'Overview':
-    st.subheader('Overview')
-    st.write("""
-    #### The data has been split into two groups:
-    - training set (train.csv):
-    The training set should be used to build your machine learning models. For the training set, we provide the outcome (also known as the “ground truth”) for each passenger. Your model will be based on “features” like passengers’ gender and class. You can also use feature engineering to create new features.
-    - test set (test.csv):
-    The test set should be used to see how well your model performs on unseen data. 
-    """)
-elif choice == 'Build Project':
-    st.subheader('Build Project')    
-    st.write('#### Data Preprocessing')    
-    st.write('#### Show data:')
-    st.table(data_new.head(5))
 
-    st.write('#### Build model and evaluation')      
-    st.write('Mean squared error: {}'.format(round(mse,2)))  
-    st.write('Root mean squared error: {}'.format(round(rmse,2)))  
-    st.write('Mean absolute error: {}'.format(round(mae,2))) 
-    st.write('R squared: {}'.format(round(r2,2))) 
-    st.table(predict_df.head(10))      
+
+
+# menu = ['Overview', 'Build Project', 'New Prediction']
+# choice = st.sidebar.selectbox('Menu', menu)
+# if choice == 'Overview':
+#     st.subheader('Overview')
+#     st.write("""
+#     #### The data has been split into two groups:
+#     - training set (train.csv):
+#     The training set should be used to build your machine learning models. For the training set, we provide the outcome (also known as the “ground truth”) for each passenger. Your model will be based on “features” like passengers’ gender and class. You can also use feature engineering to create new features.
+#     - test set (test.csv):
+#     The test set should be used to see how well your model performs on unseen data. 
+#     """)
+# elif choice == 'Build Project':
+#     st.subheader('Build Project')    
+#     st.write('#### Data Preprocessing')    
+#     st.write('#### Show data:')
+#     st.table(data_new.head(5))
+
+#     st.write('#### Build model and evaluation')      
+#     st.write('Mean squared error: {}'.format(round(mse,2)))  
+#     st.write('Root mean squared error: {}'.format(round(rmse,2)))  
+#     st.write('Mean absolute error: {}'.format(round(mae,2))) 
+#     st.write('R squared: {}'.format(round(r2,2))) 
+#     st.table(predict_df.head(10))      
     
 
-    st.write('#### Visualization')
-    fig = plt.figure(figsize=(10,10))
-    ax = fig.add_subplot(111)
+    # st.write('#### Visualization')
+    # fig = plt.figure(figsize=(10,10))
+    # ax = fig.add_subplot(111)
     
-    visualizer = PredictionError(model)
-    visualizer.fit(x_train, y_train)  # Fit the training data to the visualizer
-    visualizer.score(x_test, y_test)  # Evaluate the model on the test data
-    visualizer.ax.set_title("Prediction Error for Gradient Boosting Regressor", fontsize= 20, fontweight = 'bold')
+    # visualizer = PredictionError(model)
+    # visualizer.fit(x_train, y_train)  # Fit the training data to the visualizer
+    # visualizer.score(x_test, y_test)  # Evaluate the model on the test data
+    # visualizer.ax.set_title("Prediction Error for Gradient Boosting Regressor", fontsize= 20, fontweight = 'bold')
 
-    visualizer.ax.set_xlabel("Actual selling", fontsize= 18)
-    visualizer.ax.set_ylabel("Predicted selling", fontsize= 18)
-    visualizer.finalize()
-    xpoints = ypoints = plt.xlim()
-    plt.plot(xpoints, ypoints, linestyle='--', color='r', lw=3, scalex=False, scaley=False)
-    plt.show()
+    # visualizer.ax.set_xlabel("Actual selling", fontsize= 18)
+    # visualizer.ax.set_ylabel("Predicted selling", fontsize= 18)
+    # visualizer.finalize()
+    # xpoints = ypoints = plt.xlim()
+    # plt.plot(xpoints, ypoints, linestyle='--', color='r', lw=3, scalex=False, scaley=False)
+    # plt.show()
     
-    st_yellowbrick(visualizer)     
+    # st_yellowbrick(visualizer)     
 
-elif choice == 'New Prediction':
-    st.subheader('Make new prediction')
-    st.write('#### Input/Select data')
-    name = st.text_input('Product Name')
+# elif choice == 'New Prediction':
+#     st.subheader('Make new prediction')
+st.write('##### Input/Select data')
+name = st.text_input('Product Name')
     
-    channel = st.selectbox('DI Channel: 1, Non-DI Channel: 0 ', options=[1, 0])
-    retail = st.number_input('Insert Retail Price ($)')
-    weight = st.number_input('Insert Product Weight (lbs)')
-    dim = st.number_input('Insert Product Dim_Weight (cm3)')
-    color = st.selectbox('Insert Product Color', options=['Red','Blue','Green','Yellow','Pink','Black'])
-    size = st.selectbox('Insert Product Size', options=['Small','Medium','Large'])
-    cat = st.selectbox('Insert Category', options=['Ball','Kettelbell','Dumbbell', 'AB Mat','Roller EPP'])
+channel = st.selectbox('DI Channel: 1, Non-DI Channel: 0 ', options=[1, 0])
+retail = st.number_input('Insert Retail Price ($)')
+weight = st.number_input('Insert Weight (lbs)')
+dim = st.number_input('Insert Dim_Weight (cm3)')
+color = st.selectbox('Insert Color', options=['Red','Blue','Green','Yellow','Pink','Black'])
+size = st.selectbox('Insert Size', options=['Small','Medium','Large'])
+cat = st.selectbox('Insert Category', options=['Ball','Kettelbell','Dumbbell', 'AB Mat','Roller EPP'])
     # retail = st.slider('Retail ($)', 0.2,600.0,0.01)
     # weight = st.slider('Weight (lbs)', 0.0, 150.0, 0.01)
     # dim = st.slider('Dimension (inch3)', 0.0, 16000.0, 1.0)
     
     # make new prediction
-    st.subheader('1. Machine learning model result')
-    new_data_1 = [[retail, dim, weight, channel]]
-    prediction = model.predict(new_data_1)
-    min7 = prediction*0.94 if channel == 0 else prediction*0.73
-    max7 = prediction*1.36 if channel == 0 else prediction*1.27
-    min9 = prediction*0.73 if channel == 0 else prediction*0.6
-    max9 = prediction*1.57 if channel == 0 else prediction*1.54
-    confi = st.selectbox('Select confidence ', options=['70%', '95%'])
-    if confi == '70%':
-        st.subheader('Sell-in price of {} ranging from {} to {}'.format(name, np.round(min7,2), np.round(max7,2)))
-    else:
-        st.subheader('Sell-in price of {} ranging from {} to {}'.format(name, np.round(min9,2), np.round(max9,2)))
-
-    #Linear    
-    lr = 0.1194*weight + 0.0240*dim + 0.3380*retail + 4.9131
-    st.subheader('2. Linear regression result')
-    st.subheader('Sell-in price of {} is {}'.format(name, np.round(lr,2)))
+st.subheader('Model Result')
+new_data_1 = [[retail, dim, weight, channel]]
+prediction = model.predict(new_data_1)
+min7 = prediction*0.94 if channel == 0 else prediction*0.65
+max7 = prediction*1.36 if channel == 0 else prediction*1.19
+min8 = prediction*0.835 if channel == 0 else prediction*0.515
+max8 = prediction*1.465 if channel == 0 else prediction*1.325
+min9 = prediction*0.73 if channel == 0 else prediction*0.38
+max9 = prediction*1.57 if channel == 0 else prediction*1.46
+min99 = prediction*0.52 if channel == 0 else prediction*0.11
+max99 = prediction*1.78 if channel == 0 else prediction*1.73
+confi = st.selectbox('Select confidence ', options=['68.2%','86.6%','95%','99.7%'])
+if confi == '68.2%':
+    st.subheader('Sell-in price of {} ranging from {} to {}'.format(name, np.round(min7,2), np.round(max7,2)))
+elif confi == '86.6%':
+    st.subheader('Sell-in price of {} ranging from {} to {}'.format(name, np.round(min8,2), np.round(max8,2)))
+elif confi == '95%':
+    st.subheader('Sell-in price of {} ranging from {} to {}'.format(name, np.round(min9,2), np.round(max9,2)))    
+else:
+    st.subheader('Sell-in price of {} ranging from {} to {}'.format(name, np.round(min99,2), np.round(max99,2)))  
